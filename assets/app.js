a888927d206ffdebf2d2953eda2be5c862a6cb7c
Alex <a.mukharovskiy@gmail.com>
2022-08-12 00:43:55 +0200

    refactoring

--- assets/app.js
+++ assets/app.js
@@ -1,29 +1,29 @@
 window.onload = () => {
-    require.config({
-        'baseUrl': 'http://ui.components/assets'
-    });
-    let key = 'script[type="simplified/ui-components"]';
-    //Load knockout js
-    require(['ko']);
+  require.config({
+    baseUrl: "/assets"
+  });
+  //Load knockout js
+  require(['ko']);
 
-    function processUiNode(node)
-    {
-        for (let subNodeKey in node) {
-            let subNode = node[subNodeKey];
-            if (!subNode.hasOwnProperty('component')) {
-                throw new Error('Component is not defined for the given name ' + name);
-            }
+  let key = 'script[type="simplified/ui-components"]';
 
-            require([subNode['component']]);
+  document.querySelectorAll(key).forEach((script) => {
+    let uiData = JSON.parse(script.innerHTML);
+    processUiNode(uiData);
+  });
 
-            if (subNode.hasOwnProperty('children')) {
-                processUiNode(subNode.children);
-            }
-        }
+  function processUiNode(node) {
+    for (let subNodeKey in node) {
+      let subNode = node[subNodeKey];
+      if (!subNode.hasOwnProperty('component')) {
+        throw new Error('Component is not defined for the given name ' + name);
+      }
+
+      require([subNode['component']]);
+
+      if (subNode.hasOwnProperty('children')) {
+        processUiNode(subNode.children);
+      }
     }
-
-    document.querySelectorAll(key).forEach((script) => {
-        let uiData = JSON.parse(script.innerHTML);
-        processUiNode(uiData);
-    });
+  }
 }
