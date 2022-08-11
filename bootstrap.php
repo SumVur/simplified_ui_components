125d14de50942daf7da8d1c81f20b401fbf78120
Alex <a.mukharovskiy@gmail.com>
2022-08-12 00:44:49 +0200

    add bootstrap.php & add move some date to env

---
+++ bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+require __DIR__ .'/vendor/autoload.php';
+$config = include "./env.php";
+define('ROUTES_PATH', __DIR__ . DIRECTORY_SEPARATOR . 'routes');
+define('ASSETS_DIR', $config["ASSETS_DIR"]);
--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 
-include_once 'vendor/autoload.php';
-define('ROUTES_PATH', __DIR__ . DIRECTORY_SEPARATOR . 'routes');
-define('ASSETS_DIR', 'http://ui.components/assets/');
+include_once 'bootstrap.php';
 
 $frontController = new \Barwenock\Routes\FrontController();
 $frontController->dispatch();
